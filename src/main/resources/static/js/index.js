b59e4136e9dc1f837b0702eb806b700becd6a963
zubayer <zubayer@DESKTOP-1VUBCR6>
2019-10-03 11:18:29 +0600

    Update git ignore

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -3,8 +3,16 @@ $(document).ready(function(){
 	var $image = $('#image');
 
 	$image.cropper({
-		aspectRatio: 16 / 9,
-		
+		//aspectRatio: 16 / 9,
+		crop(event){
+//			console.log("start - x : " + event.detail.x);
+//			console.log("start - y : " + event.detail.y);
+//			console.log("Width : " + event.detail.width);
+//			console.log("Height : " + event.detail.height);
+//			console.log("Rotate : " + event.detail.rotate);
+			console.log(event.detail.scaleX);
+		    console.log(event.detail.scaleY);
+		}
 	});
 
 
