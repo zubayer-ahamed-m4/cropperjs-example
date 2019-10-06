14d0136fa5cf5ee08611475e6cee3efc262e528d
zubay <zubay@DESKTOP-FR0HQC8>
2019-10-06 20:42:04 +0600

    06-10-2019

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,20 +1,37 @@
 $(document).ready(function(){
 
 	var $image = $('#image');
-
-	$image.cropper({
-		//aspectRatio: 16 / 9,
-		preview: ".cropped-imaged-preview",
-		crop(event){
-//			console.log("start - x : " + event.detail.x);
-//			console.log("start - y : " + event.detail.y);
-//			console.log("Width : " + event.detail.width);
-//			console.log("Height : " + event.detail.height);
-//			console.log("Rotate : " + event.detail.rotate);
-			console.log(event.detail.scaleX);
-		    console.log(event.detail.scaleY);
+	var options = {
+		aspectRatio : 16 / 9,
+		preview : ".cropped-imaged-preview",
+		viewMode : 2,
+		ready : function(e){
+			console.log(e);
+		},
+		cropstart : function(e){
+			console.log(e);
+		},
+		cropmove : function(e){
+			console.log(e);
+		},
+		cropend : function(e){
+			console.log(e);
+		},
+		crop : function(e){
+			console.log(e);
+		},
+		zoome : function(e){
+			console.log(e);
 		}
-	});
+	}
+
+	var aspectRatio = $('.aspect-ratio');
+
+	$image.cropper(options);
+	
+	$('.aspect-ratio').on('click', function(e){
+		console.log($(this).html());
+	})
 
 
 })
